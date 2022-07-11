d0ec8371b04ff594febc267f910f3dbefeeba3d6
Shaw Lu <shawlu95@gmail.com>
2022-07-11 09:33:47 -0700

    Have trouble finding /socket.io/socket.io.js

--- index.js
+++ index.js
@@ -1,8 +1,19 @@
 const path = require('path');
+const http = require('http');
 const express = require('express');
+const socketio = require('socket.io');
 
 const app = express();
+const server = http.createServer(app);
+const io = socketio(server);
+
+// Set static folder
 app.use(express.static(path.join(__dirname, 'public')));
+
+io.on('connection', (socket) => {
+  console.log('new connection');
+  socket.emit('message', 'Welcome to the chat!');
+});
 
 const PORT = process.env.PORT || 8080;
 app.listen(PORT, () => console.log('listening on port:', PORT));
