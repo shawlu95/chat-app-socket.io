0a7f6c08394a6db913d6ce30529aea459ad1d77e
Shaw Lu <shawlu95@gmail.com>
2022-07-13 08:46:41 -0700

    UI improvement

--- public/js/main.js
+++ public/js/main.js
@@ -1,13 +1,22 @@
-const chatForm = document.getElementById('chat-form');
 const socket = io();
+const chatForm = document.getElementById('chat-form');
+const chatMessages = document.querySelector('.chat-messages');
 
 socket.on('message', (message) => {
   console.log(message);
+  outputMessage(message);
+  chatMessages.scrollTop = chatMessages.scrollHeight;
 });
 
 // message submit
 chatForm.addEventListener('submit', (e) => {
   e.preventDefault();
   const msg = e.target.elements.msg.value;
+
+  // Emit message to server
   socket.emit('chatMessage', msg);
+
+  // Clear input
+  e.target.elements.msg.value = '';
+  e.target.elements.msg.focus();
 });
