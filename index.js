b5e3969adb2614c355683d4defb0cfc0ac76607b
Shaw Lu <shawlu95@gmail.com>
2022-07-13 08:50:56 -0700

    Turn message into object with username and time

--- index.js
+++ index.js
@@ -2,25 +2,34 @@ const path = require('path');
 const http = require('http');
 const express = require('express');
 const socketio = require('socket.io');
+const formatMessage = require('./utils/messages');
 
 const app = express();
 const server = http.createServer(app);
 const io = socketio(server);
-
+const botName = 'Bailey';
 // Set static folder
 app.use(express.static(path.join(__dirname, 'public')));
 
 io.on('connection', (socket) => {
   console.log('new connection');
-  socket.emit('message', 'Welcome to the chat!');
-  socket.broadcast.emit('message', 'A user entered the chat!');
+  socket.emit('message', formatMessage(botName, 'Welcome to the chat!'));
+
+  // broadcast: send to all clinets except the emitter
+  socket.broadcast.emit(
+    'message',
+    formatMessage(botName, 'A user entered the chat!')
+  );
   socket.on('disconnect', () => {
-    socket.broadcast.emit('message', 'A user left the chat!');
+    socket.broadcast.emit(
+      'message',
+      formatMessage(botName, 'A user left the chat!')
+    );
   });
 
   // listening for chat message
   socket.on('chatMessage', (msg) => {
-    io.emit('message', msg);
+    io.emit('message', formatMessage('User', msg));
   });
 });
 
--- public/js/main.js
+++ public/js/main.js
@@ -20,3 +20,18 @@ chatForm.addEventListener('submit', (e) => {
   e.target.elements.msg.value = '';
   e.target.elements.msg.focus();
 });
+
+function outputMessage(message) {
+  const div = document.createElement('div');
+  div.classList.add('message');
+  const p = document.createElement('p');
+  p.classList.add('meta');
+  p.innerText = message.username;
+  p.innerHTML += `<span>${message.time}</span>`;
+  div.appendChild(p);
+  const para = document.createElement('p');
+  para.classList.add('text');
+  para.innerText = message.text;
+  div.appendChild(para);
+  document.querySelector('.chat-messages').appendChild(div);
+}
